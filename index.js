371dfd82f4470178d927cfccd0b3462c9ed52045
Ismeny Saguilan <menysag2@gmail.com>
2022-01-11 11:23:28 -0600

    created function declaration

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// arrow function to choose rock, paper, or scissors
+// arrow function FOR USER to choose rock, paper, or scissors
 const getUserChoice = userInput =>  {
     // converts user input to lowercase so it can match and perform correctly
     userInput = userInput.toLowerCase();
@@ -11,3 +11,20 @@ const getUserChoice = userInput =>  {
 };
 // should print 'paper'
 console.log(getUserChoice('PAPER'))
+
+//function FOR COMPUTER to choose rock, paper, or scissors
+function getComputerChoice() {
+    // get a random whole number between 0 and 2
+    Math.random(Math.floor() * 3)
+    // then depending on the number return either rock, paper or scissors
+    switch (0) {
+        case 0: 
+         return 'rock';
+        case 1:
+         return 'paper';
+        case 2: 
+         return 'scissors'
+    }
+}
+//should print 'rock'
+console.log(getComputerChoice());
